814cbc82e7df4c7368fa2bc7475035e0c54c3f83
Ivan Okello <74784829+Ivandamien@users.noreply.github.com>
2023-05-16 18:44:04 +0300

    Working with assets

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,1 +1,65 @@
-import blog-1 from './blog-1.jpg';
+import blog1 from './blog-1.jpg';
+import blog2 from './blog-2.jpg';
+import blog3 from './blog-3.jpg';
+import brand1 from './brand-1.png';
+import brand2 from './brand-2.png';
+import brand3 from './brand-3.png';
+import brand4 from './brand-4.png';
+import brand5 from './brand-5.png';
+import brand6 from './brand-6.png';
+import featuredGame1 from './featured-game-1.jpg';
+import featuredGame2 from './featured-game-2.jpg';
+import featuredGame3 from './featured-game-3.jpg';
+import featuredGame4 from './featured-game-4.jpg';
+import featuredGameIcon from './featured-game-icon.png';
+import footerBottomImg from './footer-bottom-img.png';
+import headerTopBg from './header-top-bg.jpg';
+import heroBanner from './hero-banner.png';
+import heroBg from './hero-bg.jpg';
+import latestGame1 from './latest-game-1.jpg';
+import latestGame2 from './latest-game-2.jpg';
+import latestGame3 from './latest-game-3.jpg';
+import liveMatchBanner from './live-match-banner.jpg';
+import liveMatchPlayer1 from './live-match-player-1.png';
+import liveMatchPlayer2 from './live-match-player-2.png';
+import sectionWrapperBg from './section-wrapper-bg.png';
+import shopBg from './shop-bg.jpg';
+import shopImg1 from './shop-img-1.jpg';
+import shopImg2 from './shop-img-2.jpg';
+import shopImg3 from './shop-img-3.jpg';
+import titleUnderline from './title-underline.png';
+
+
+export {
+    blog1,
+    blog2,
+    blog3,
+    brand1,
+    brand2,
+    brand3,
+    brand4,
+    brand5,
+    brand6,
+    featuredGame1,
+    featuredGame2,
+    featuredGame3,
+    featuredGame4,
+    featuredGameIcon,
+    footerBottomImg,
+    headerTopBg,
+    heroBanner,
+    heroBg,
+    latestGame1,
+    latestGame2,
+    latestGame3,
+    liveMatchBanner,
+    liveMatchPlayer1,
+    liveMatchPlayer2,
+    sectionWrapperBg,
+    shopBg,
+    shopImg1,
+    shopImg2,
+    shopImg3,
+    titleUnderline
+    
+}
