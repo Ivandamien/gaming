26c4b914585411ac23f6543302b8fbbc75334551
Ivan Okello <74784829+Ivandamien@users.noreply.github.com>
2023-05-18 15:02:09 +0300

    Getting started on the Blog site

---
+++ src/components/Blog.js
@@ -0,0 +1,78 @@
+import React from 'react';
+
+const Blog = () => {
+  const articles = [
+    {
+      id: 1,
+      image: './assets/images/blog-1.jpg',
+      author: 'Admin',
+      date: 'September 19, 2022',
+      title: 'Shooter Action Video',
+      excerpt: 'Compete With 100 Players On A Remote Island Thats Winner Takes Showdown Known Issue.'
+    },
+    {
+      id: 2,
+      image: './assets/images/blog-2.jpg',
+      author: 'Admin',
+      date: 'September 19, 2022',
+      title: 'The Walking Dead',
+      excerpt: 'Compete With 100 Players On A Remote Island Thats Winner Takes Showdown Known Issue.'
+    },
+    {
+      id: 3,
+      image: './assets/images/blog-3.jpg',
+      author: 'Admin',
+      date: 'September 19, 2022',
+      title: 'Defense Of The Ancients',
+      excerpt: 'Compete With 100 Players On A Remote Island Thats Winner Takes Showdown Known Issue.'
+    }
+  ];
+
+  return (
+    <section className="section blog" id="blog" aria-label="blog">
+      <div className="container">
+        <h2 className="h2 section-title">
+          Latest News &amp; <span className="span">Articles</span>
+        </h2>
+        <p className="section-text">
+          Compete With 100 Players On A Remote Island For Winner Takes Showdown Known Issue Where Certain Skin Strategic
+        </p>
+        <ul className="blog-list">
+          {articles.map(article => (
+            <li key={article.id}>
+              <div className="blog-card">
+                <figure className="card-banner img-holder" style={{ '--width': 400, '--height': 290 }}>
+                  <img src={article.image} width="400" height="290" loading="lazy" alt={article.title} className="img-cover" />
+                </figure>
+                <div className="card-content">
+                  <ul className="card-meta-list">
+                    <li className="card-meta-item">
+                      <ion-icon name="person-outline"></ion-icon>
+                      <a href="#" className="item-text">{article.author}</a>
+                    </li>
+                    <li className="card-meta-item">
+                      <ion-icon name="calendar-outline"></ion-icon>
+                      <time dateTime="2022-09-19" className="item-text">{article.date}</time>
+                    </li>
+                  </ul>
+                  <h3>
+                    <a href="#" className="card-title">{article.title}</a>
+                  </h3>
+                  <p className="card-text">
+                    {article.excerpt}
+                  </p>
+                  <a href="#" className="card-link">
+                    <span className="span">Read More</span>
+                    <ion-icon name="caret-forward"></ion-icon>
+                  </a>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default Blog;
