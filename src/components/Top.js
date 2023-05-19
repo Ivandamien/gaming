ae06050bca256234549027c0d9b5b84601057705
Ivan Okello <74784829+Ivandamien@users.noreply.github.com>
2023-05-19 14:39:16 +0300

    Building the to button again, just some dom manipulation shit

--- src/components/Top.js
+++ src/components/Top.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Top = () => {
+  const [showBackToTop, setShowBackToTop] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >= 200) {
+        setShowBackToTop(true);
+      } else {
+        setShowBackToTop(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   const handleScrollToTop = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
   return (
-    <a href="#top" className="back-top-btn" aria-label="back to top" data-back-top-btn onClick={handleScrollToTop}>
-      <ion-icon name="caret-up"></ion-icon>
-    </a>
+    showBackToTop && (
+      <Link to="#top" className="back-top-btn" aria-label="back to top" data-back-top-btn onClick={handleScrollToTop}>
+        <ion-icon name="caret-up"></ion-icon>
+      </Link>
+    )
   );
 };
 
